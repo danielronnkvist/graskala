3f2845192aac51562dc4d644b2af144ede20538e
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-04-08 22:34:40 +0200

    Show post route declared

--- imports/ui/Post.jsx
+++ imports/ui/Post.jsx
@@ -1,3 +1,4 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
 import React, { Component, PropTypes } from 'react';
 
 import { Posts } from '../api/posts.js';
@@ -7,7 +8,12 @@ export default class Post extends Component {
   render() {
     return (
       <div className="post">
-        <h1>{this.props.post.title}</h1>
+        <h1
+          onClick={ () =>
+            FlowRouter.go('/post/:slug', this.props.post)
+          }>
+          {this.props.post.title}
+        </h1>
         <small>{this.props.post.createdAt.toDateString()}</small>
       </div>
     );
