050ed52f59648fc903ddc76e71ca7f027e3bbf00
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-04-14 21:09:40 +0200

    Check if google settings exist, otherwise use the env variables

--- server/accounts.js
+++ server/accounts.js
@@ -1,10 +1,15 @@
 import { Accounts } from 'meteor/accounts-google';
 
+let settings = Meteor.settings.google ? Meteor.settings.google : {
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  secret: process.env.GOOGLE_SECRET,
+}
+
 ServiceConfiguration.configurations.remove({
   service: 'google',
 })
 ServiceConfiguration.configurations.insert({
   service: "google",
-  clientId: Meteor.settings.google.clientId || process.env.GOOGLE_CLIENT_ID,
-  secret: Meteor.settings.google.secret || process.env.GOOGLE_SECRET,
+  clientId: settings.clientId,
+  secret: settings.secret,
 });
