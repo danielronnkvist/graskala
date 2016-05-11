55ab1dd838e3ee2161dfca83800540f28e567615
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-05-11 20:43:09 +0200

    Use env variables instead of settings.json for easier heroku integration

--- server/accounts.js
+++ server/accounts.js
@@ -1,15 +1,10 @@
 import { Accounts } from 'meteor/accounts-google';
 
-let settings = Meteor.settings.google ? Meteor.settings.google : {
-  clientId: process.env.GOOGLE_CLIENT_ID,
-  secret: process.env.GOOGLE_SECRET,
-}
-
 ServiceConfiguration.configurations.remove({
   service: 'google',
-})
+});
 ServiceConfiguration.configurations.insert({
   service: "google",
-  clientId: settings.clientId,
-  secret: settings.secret,
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  secret: process.env.GOOGLE_SECRET,
 });
