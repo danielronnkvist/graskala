658aaa2dcf42276c3c44b572c0333f7b556c51ad
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-08-19 22:54:02 +0200

    Logic to make sure a whole lot of pages isn't displayed at the same time

--- imports/ui/misc/Pagination.jsx
+++ imports/ui/misc/Pagination.jsx
@@ -7,16 +7,36 @@ import { Posts } from './../../../lib/collections.js';
 class Pagination extends Component {
 
   getButtons(page, pages) {
-    return Array.apply(null, Array(pages)).map((p, i) => {
+    let nextButton = [(
+      <div key={`last`}
+           className={`page-button`}
+           onClick={ () => FlowRouter.setQueryParams({page:page+1}) }>
+        →
+      </div>
+    )];
+    if(page === pages) nextButton = [];
+
+    return Array.apply(null, Array(pages)).reduce((array, _, i) => {
       i += 1;
-      return (
-        <div key={i}
-             className={`page-button ${i == page ? 'active' : ''}`}
-             onClick={ () => FlowRouter.setQueryParams({page:i}) }>
-          {i}
-        </div>
-      );
-    });
+
+      if(pages < 6 || Math.abs(i - page) < 3 || i === 1 || i === pages) {
+        array.push(
+          <div key={i}
+               className={`page-button ${i == page ? 'active' : ''}`}
+               onClick={ () => FlowRouter.setQueryParams({page:i}) }>
+            {i}
+          </div>
+        );
+      } else if(Math.abs(i - page) === 3) {
+        array.push(
+          <div key={i}
+               className={`page-button`}>
+            ...
+          </div>
+        )
+      }
+      return array;
+    }, []).concat(nextButton);
   }
 
   render() {
@@ -24,6 +44,7 @@ class Pagination extends Component {
       numberOfPosts,
       page,
     } = this.props;
+    page *= 1;
     let pages = Math.ceil(numberOfPosts / 5);
 
     return (
