2939dac3e4948f0709b28e87503658ff824d95db
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-08-21 18:36:58 +0200

    Limit which emails that are allowed to login

--- server/accounts.js
+++ server/accounts.js
@@ -1,4 +1,4 @@
-import { Accounts } from 'meteor/accounts-google';
+import { Accounts } from 'meteor/accounts-base';
 
 ServiceConfiguration.configurations.remove({
   service: 'google',
@@ -7,4 +7,13 @@ ServiceConfiguration.configurations.insert({
   service: "google",
   clientId: process.env.GOOGLE_CLIENT_ID,
   secret: process.env.GOOGLE_SECRET,
+});
+
+Accounts.validateNewUser(function(user) {
+  let validEmails = JSON.parse(process.env.VALID_EMAILS);
+  if (validEmails.indexOf(user.emails[0].address.toLowerCase())) {
+    return true;
+  } else {
+    throw new Meteor.Error(403, "Email not allowed.");
+  }
 });
