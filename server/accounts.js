f8fdafb0d7848f8b90713d02015022c1f376dce1
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-10-01 17:15:40 +0200

    Check if the index is greater than -1

--- server/accounts.js
+++ server/accounts.js
@@ -10,8 +10,8 @@ ServiceConfiguration.configurations.insert({
 });
 
 Accounts.validateNewUser(function(user) {
-  let validEmails = JSON.parse(process.env.VALID_EMAILS);
-  if (validEmails.indexOf(user.emails[0].address.toLowerCase())) {
+  let validEmails = process.env.VALID_EMAILS;
+  if (validEmails.indexOf(user.services.google.email.toLowerCase()) > -1) {
     return true;
   } else {
     throw new Meteor.Error(403, "Email not allowed.");
