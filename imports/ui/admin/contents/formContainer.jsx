01e66825cebbf7b5ad60eb58d3b6e8e858db976e
Daniel Rönnkvist <daniel@ronnkvist.com>
2016-10-02 16:53:51 +0200

    Made the about and contact form containers into a general component

--- imports/ui/admin/contents/formContainer.jsx
+++ imports/ui/admin/contents/formContainer.jsx
@@ -1,12 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import PageForm from './pageForm.jsx';
-import { Contact } from './../../../../lib/collections.js';
 
-export default class ContactForm extends Component {
+export default class FormContainer extends Component {
 
   saveData(data, id) {
     return new Promise((resolve, reject) => {
-      Contact.update(id, {
+      this.props.collection.update(id, {
         $set: {
           title: data.title,
           text: data.text,
@@ -20,13 +19,14 @@ export default class ContactForm extends Component {
   render() {
     const {data} = this.props;
 
-    return <PageForm data={data} saveData={this.saveData} />
+    return <PageForm data={data} saveData={this.saveData.bind(this)} />
   }
 
 }
 
-ContactForm.propTypes = {
+FormContainer.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
   data: PropTypes.object.isRequired,
+  collection: PropTypes.object.isRequired,
 };
